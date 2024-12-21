f25004f0e5b56afeb9bb29730a3e68ec386ed962
josie <joshuagregory730@gmail.com>
2024-12-21 12:58:25 +0000

    generates and checks for ids when making an account so it doesnt use the same on as someone else

--- program.c
+++ program.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// functions
 
 void createAccount();
 void login();
+int generateID();
 
 int main(){
     char account_exists;
     bool signed_in = false;
+
+    printf("%d",time(0));
 
     printf("Welcome to the Libellant Account Manager. Do you have an existing account? (Y/N)\n");
     do{
@@ -20,6 +28,7 @@ int main(){
             case 'N':
                 printf("Initialising account creation sequence..\n");
                 createAccount();
+                signed_in = true;
                 break;
             default:
                 printf("Input not recognised. Please try again.\n");
@@ -31,15 +40,56 @@ int main(){
 }
 
 void createAccount(){
+
     char username[25];
     char password[25];
+    long long userID;
+    long long check_for_existing_ID;
+    bool generatingID = false;
 
     printf("\nEnter a username (25 characters max): ");
     scanf("%s",username);
     printf("Enter a password (25 characters max): ");
     scanf("%s",password);
+    printf("Generating user ID..");
+
+    do{ // generate id and check if its already being used
+        generatingID = false;
+        userID = generateID();
+
+        FILE *Fp = fopen("id_list.txt", "r");
+
+        if(Fp == NULL){
+            printf("An error has occured.\n");
+            return;
+        }
+        else{
+            while(fscanf(Fp, "%d", &check_for_existing_ID) == 1){
+                if(check_for_existing_ID == userID){
+                    generatingID = true;
+                    break;
+                }
+            }
+        }
+
+        if(!generatingID){
+            FILE *Fp = fopen("id_list.txt", "a");
+            fprintf(Fp, "%d\n",userID);
+        }
+
+        fclose(Fp);
+
+        printf("%0.7d\n",userID);
+    }while(generatingID);
+
 }
 
 void login(){
 
+}
+
+int generateID(){
+    srand(time(0));
+    long long id = (rand() % 10000000);
+    return id;
 }
