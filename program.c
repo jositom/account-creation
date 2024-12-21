9b436b34be937f322c4c1f94dd1a749fc57e5140
josie <joshuagregory730@gmail.com>
2024-12-21 15:57:17 +0000

    account creation is now possible. account details are stored in a .txt file.

--- program.c
+++ program.c
@@ -28,7 +28,7 @@ int main(){
             case 'N':
                 printf("Initialising account creation sequence..\n");
                 createAccount();
-                signed_in = true;
+                return 0;
                 break;
             default:
                 printf("Input not recognised. Please try again.\n");
@@ -51,20 +51,20 @@ void createAccount(){
     scanf("%s",username);
     printf("Enter a password (25 characters max): ");
     scanf("%s",password);
-    printf("Generating user ID..");
+    printf("Generating user ID..\n");
 
     do{ // generate id and check if its already being used
         generatingID = false;
         userID = generateID();
 
-        FILE *Fp = fopen("id_list.txt", "r");
+        FILE *ID_list = fopen("data/id_list.txt", "r");
 
-        if(Fp == NULL){
+        if(ID_list == NULL){
             printf("An error has occured.\n");
             return;
         }
         else{
-            while(fscanf(Fp, "%d", &check_for_existing_ID) == 1){
+            while(fscanf(ID_list, "%d", &check_for_existing_ID) == 1){
                 if(check_for_existing_ID == userID){
                     generatingID = true;
                     break;
@@ -73,15 +73,31 @@ void createAccount(){
         }
 
         if(!generatingID){
-            FILE *Fp = fopen("id_list.txt", "a");
-            fprintf(Fp, "%d\n",userID);
+            FILE *ID_list = fopen("data/id_list.txt", "a");
+            fprintf(ID_list, "%d\n",userID);
         }
 
-        fclose(Fp);
+        fclose(ID_list);
 
-        printf("%0.7d\n",userID);
     }while(generatingID);
 
+    printf("User ID generated! (%0.7d)\n",userID);
+
+    printf("Saving details..\n");
+
+    char file_name[100];
+
+    sprintf(file_name, "data/accounts/%d.txt", userID);
+
+    FILE *account_details = fopen(file_name, "w");
+
+    fprintf(account_details, "%s\n",username);
+    fprintf(account_details, "%s\n",password);
+    fprintf(account_details, "%d\n",userID);
+
+    fclose(account_details);
+
+    printf("Details saved! You may now log in to your account. Make sure to copy your user ID so you can log in.\n")
 }
 
 void login(){
